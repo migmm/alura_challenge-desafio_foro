764ea3e4a930ce5604a9813c7768f26dc06a7c7d
Miguel <micmigm@gmail.com>
2024-12-23 18:23:19 -0300

    Created topic entitiy

---
+++ src/main/java/com/alura/foro/model/Topic.java
@@ -0,0 +1,36 @@
+package com.alura.foro.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String course;
+}
