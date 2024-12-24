358e76aa37acbcf4a54f41259c25a67183677fd1
Miguel <micmigm@gmail.com>
2024-12-24 10:16:57 -0300

    Added getters and setters and removed data

--- src/main/java/com/alura/foro/dto/UserDTO.java
+++ src/main/java/com/alura/foro/dto/UserDTO.java
@@ -1,9 +1,11 @@
 package com.alura.foro.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-
+@Getter
+@Setter
 public class UserDTO {
     private String username;
     private String password;
