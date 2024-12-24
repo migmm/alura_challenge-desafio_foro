b63fed10a48eb45853bd1036e18c56f84981fa7b
Miguel <micmigm@gmail.com>
2024-12-24 10:16:50 -0300

    Added getters and setters and removed data

--- src/main/java/com/alura/foro/dto/TopicDTO.java
+++ src/main/java/com/alura/foro/dto/TopicDTO.java
@@ -1,9 +1,11 @@
 package com.alura.foro.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-
-@Data
+@Getter
+@Setter
 public class TopicDTO {
     private String title;
     private String message;
