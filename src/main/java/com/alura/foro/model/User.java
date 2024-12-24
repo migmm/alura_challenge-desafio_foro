03a6605f737237c655cac8e7c4c08cc27afbff8d
Miguel <micmigm@gmail.com>
2024-12-24 10:17:20 -0300

    Added getters and setters

--- src/main/java/com/alura/foro/model/User.java
+++ src/main/java/com/alura/foro/model/User.java
@@ -1,12 +1,11 @@
 package com.alura.foro.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
