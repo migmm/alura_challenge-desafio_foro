aa7744f676c5d3a666253b2e6b8f4bb509a6fd3f
Miguel <micmigm@gmail.com>
2024-12-26 20:56:02 -0300

    Added a find

--- src/main/java/com/alura/foro/repository/TopicRepository.java
+++ src/main/java/com/alura/foro/repository/TopicRepository.java
@@ -2,6 +2,8 @@ package com.alura.foro.repository;
 
 import com.alura.foro.model.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Optional <Topic> findByTituloAndMensaje(String titulo, String mensaje);
 }
