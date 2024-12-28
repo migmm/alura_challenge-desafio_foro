751220ec6355a77cc8d61b6c4b915069ba2719aa
Miguel <micmigm@gmail.com>
2024-12-27 22:18:48 -0300

    Created JWT token filter

---
+++ src/main/java/com/alura/foro/security/JwtTokenFilter.java
@@ -0,0 +1,46 @@
+package com.alura.foro.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    public JwtTokenFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = extractToken(request);
+
+        if (token != null) {
+            String username = tokenService.validateToken(token);
+            if (username != null) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
