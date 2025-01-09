a6a59d728aa9f47d8188718ee30ff6ee73a65efa
Miguel <micmigm@gmail.com>
2025-01-09 20:53:40 -0300

    Added validations to user dto

--- src/main/java/com/alura/foro/dto/UserDTO.java
+++ src/main/java/com/alura/foro/dto/UserDTO.java
@@ -1,11 +1,19 @@
 package com.alura.foro.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
+
+    @NotEmpty(message = "El nombre de usuario no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres")
     private String username;
+
+    @NotEmpty(message = "La contraseña no puede estar vacía")
+    @Size(min = 6, max = 100, message = "La contraseña debe tener entre 6 y 100 caracteres")
     private String password;
 }
