d8c4c6978afb4eb8fba8c7e2ab2120988bc1e408
Miguel <micmigm@gmail.com>
2025-01-09 20:54:06 -0300

    Removed unused method

--- src/main/java/com/alura/foro/repository/TopicRepository.java
+++ src/main/java/com/alura/foro/repository/TopicRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    Optional <Topic> findByTituloAndMensaje(String titulo, String mensaje);
 }
