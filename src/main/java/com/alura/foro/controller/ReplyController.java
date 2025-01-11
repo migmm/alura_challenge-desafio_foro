a7b9acc9ab1cb898eb49dbe3c9f690132dd4f5cd
Miguel <micmigm@gmail.com>
2025-01-11 20:35:43 -0300

    Refactored controller

--- src/main/java/com/alura/foro/controller/ReplyController.java
+++ src/main/java/com/alura/foro/controller/ReplyController.java
@@ -3,52 +3,30 @@ package com.alura.foro.controller;
 import com.alura.foro.dto.ReplyDTO;
 import com.alura.foro.model.Reply;
 import com.alura.foro.service.ReplyService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/replies")
+@RequestMapping("/respuestas")
 public class ReplyController {
 
     @Autowired
     private ReplyService replyService;
 
-    // Crear una nueva respuesta
     @PostMapping
     public ResponseEntity<Reply> createReply(@Valid @RequestBody ReplyDTO replyDTO) {
-        Reply createdReply = replyService.createReply(replyDTO);
-        return new ResponseEntity<>(createdReply, HttpStatus.CREATED);
+        return ResponseEntity.ok(replyService.createReply(replyDTO));
     }
 
-    // Obtener todas las respuestas
-    @GetMapping
-    public ResponseEntity<List<Reply>> getAllReplies() {
-        List<Reply> replies = replyService.getAllReplies();
-        return ResponseEntity.ok(replies);
+    @GetMapping("/topico/{topicId}")
+    public ResponseEntity<List<Reply>> getRepliesByTopic(@PathVariable Long topicId) {
+        return ResponseEntity.ok(replyService.getRepliesByTopic(topicId));
     }
 
-    // Obtener una respuesta por ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Reply> getReplyById(@PathVariable Long id) {
-        Optional<Reply> reply = replyService.getReplyById(id);
-        return reply.map(ResponseEntity::ok)
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    // Actualizar una respuesta
-    @PutMapping("/{id}")
-    public ResponseEntity<Reply> updateReply(@PathVariable Long id, @Valid @RequestBody ReplyDTO replyDTO) {
-        Reply updatedReply = replyService.updateReply(id, replyDTO);
-        return ResponseEntity.ok(updatedReply);
-    }
-
-    // Eliminar una respuesta
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReply(@PathVariable Long id) {
         replyService.deleteReply(id);
