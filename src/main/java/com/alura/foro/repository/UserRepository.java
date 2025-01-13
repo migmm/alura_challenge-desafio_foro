8083ff2d9e9b53c296e82f388a9ff7292f089ea1
Miguel <micmigm@gmail.com>
2025-01-13 16:35:37 -0300

    Added new find

--- src/main/java/com/alura/foro/repository/UserRepository.java
+++ src/main/java/com/alura/foro/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.alura.foro.repository;
 
 import com.alura.foro.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
