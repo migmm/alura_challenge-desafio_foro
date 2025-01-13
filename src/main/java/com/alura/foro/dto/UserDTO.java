5ca6f6092d10fb317dcd8f6ae00bf5fa6091dcc1
Miguel <micmigm@gmail.com>
2025-01-13 16:34:04 -0300

    Simplified DTO

--- src/main/java/com/alura/foro/dto/UserDTO.java
+++ src/main/java/com/alura/foro/dto/UserDTO.java
@@ -1,19 +1,15 @@
 package com.alura.foro.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
-
-    @NotEmpty(message = "El nombre de usuario no puede estar vacío")
-    @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres")
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
     private String username;
 
-    @NotEmpty(message = "La contraseña no puede estar vacía")
-    @Size(min = 6, max = 100, message = "La contraseña debe tener entre 6 y 100 caracteres")
+    @NotBlank(message = "La contraseña no puede estar vacía")
     private String password;
 }
