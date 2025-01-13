7ac835cc7631c8200fbc837c3abeb7b5a1e3c877
Miguel <micmigm@gmail.com>
2025-01-13 16:35:12 -0300

    Added new find

--- src/main/java/com/alura/foro/repository/ReplyRepository.java
+++ src/main/java/com/alura/foro/repository/ReplyRepository.java
@@ -2,6 +2,11 @@ package com.alura.foro.repository;
 
 import com.alura.foro.model.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findByTopicId(Long topicId);
 }
